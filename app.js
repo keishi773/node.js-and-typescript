bfc2491c5d3ed721e7633c8b4894d5f2b23f62ac
藤定慶史 <fujitadashikeishi@keshi.local>
2020-12-03 21:17:38 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const http = require("http");
+class Main {
+    constructor() {
+        // httpサーバーを設定する
+        const server = http.createServer((request, response) => this.requestHandler(request, response));
+        // サーバーを起動してリクエストを待ち受け状態にする
+        server.listen("5000");
+        // ログを出力する
+        console.log("http://localhost:5000へアクセスください");
+    }
+    //サーバーにリクエストがあった時に実行される関数
+    requestHandler(request, response) {
+        response.end("Hello! Node.js with TypeScript");
+    }
+}
+new Main();
+//# sourceMappingURL=app.js.map
