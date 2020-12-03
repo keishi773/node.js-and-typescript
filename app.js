4f18b28984048aa3d9428f988f0b462f49815a23
藤定慶史 <fujitadashikeishi@keshi.local>
2020-12-03 22:10:05 +0900

    http server

--- app.js
+++ app.js
@@ -1,21 +1,119 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const http = require("http");
+const fs = require("fs");
+const path = require("path");
 class Main {
     constructor() {
+        // ルートフォルダーを指定
+        this._rootFolder = "/";
         // httpサーバーを設定する
-        const server = http.createServer((request, response) => this.requestHandler(request, response));
-        // サーバーを起動してリクエストを待ち受け状態にする
-        server.listen("5000");
-        // ログを出力する
-        console.log("http://localhost:5000 へアクセスください");
+        const server = http.createServer((request, response) => this.requestListener(request, response));
+        server.listen(process.env.PORT || 5000, () => this.listeningHandler());
     }
     /**
-     * サーバーにリクエストがあった時に実行される関数
+     * httpサーバーが待ち受け状態になった時に実行される関数
      */
-    requestHandler(request, response) {
-        response.end("Hello! Node.js with TypeScript");
+    listeningHandler() {
+        console.log((process.env.PORT || 5000) + "でhttpサーバーが待ち受け状態です");
+    }
+    requestListener(request, response) {
+        // リクエストがあったファイル
+        let requestURL = request.url;
+        // リクエストのあったファイルの拡張子を取得
+        const extensionName = path.extname(requestURL);
+        let contentType;
+        let isBinary;
+        //ファイルの拡張子に応じてルーティング処理
+        if (extensionName != "") {
+            switch (extensionName) {
+                case ".html":
+                    contentType = "text/html";
+                    isBinary = false;
+                    break;
+                case ".css":
+                    contentType = "text/css";
+                    isBinary = false;
+                    break;
+                case ".js":
+                case ".ts":
+                    contentType = "text/javascript";
+                    isBinary = false;
+                    break;
+                case ".png":
+                    contentType = "image/png";
+                    isBinary = true;
+                    break;
+                case ".jpg":
+                    contentType = "image/jpeg";
+                    isBinary = true;
+                    break;
+                case ".gif":
+                    contentType = "image/gif";
+                    isBinary = true;
+                    break;
+                case ".swf":
+                    contentType = "application/x-shockwave-flash";
+                    isBinary = true;
+                    break;
+                default:
+                    // どこにも該当しない場合は、index.htmlを読み込む
+                    requestURL = "index.html";
+                    contentType = "text/html";
+                    isBinary = false;
+                    break;
+            }
+        }
+        else {
+            //　拡張子が存在しない場合は、index.htmlを読み込む
+            requestURL = "index.html";
+            contentType = "text/html";
+            isBinary = false;
+        }
+        this.readFileHandler(requestURL, contentType, isBinary, response);
+    }
+    readFileHandler(fileName, contentType, isBinary, response) {
+        // ファイルの場所
+        const filePath = __dirname + this._rootFolder + fileName;
+        // filePathが存在するかどうかを調べる。存在している場合はexistにtrueが入る。
+        const exist = fs.existsSync(filePath);
+        this.responseHandler(exist, filePath, contentType, isBinary, response);
+    }
+    /**
+     * レスポンスを返す処理
+     */
+    responseHandler(exist, filePath, contentType, isBinary, response) {
+        if (exist) {
+            // ファイルを読み込む際のエンコード指定
+            const encoding = !isBinary ? "utf8" : "binary";
+            // ファイルの読み込み
+            fs.readFile(filePath, { encoding: encoding }, (error, data) => this.fileReadHandler(error, data, contentType, isBinary, response));
+        }
+        else {
+            //　ファイルが存在しない場合は400エラーを返す
+            response.statusCode = 400;
+            response.end("400 Error");
+        }
+    }
+    /**
+     * ファイルの読み込みが完了した時に実行される処理
+     */
+    fileReadHandler(error, data, contentType, isBinary, response) {
+        if (error) {
+            response.statusCode = 500; // レスポンスデータにステータスコード500を設定
+            response.end("Internal Server Error");
+        }
+        else {
+            response.statusCode = 200; // レスポンスデータにステータスコード200を設定
+            response.setHeader("Content-Type", contentType); // レスポンスデータのヘッダーにContent-Typeを設定
+            if (!isBinary) {
+                response.end(data);
+            }
+            else {
+                response.end(data, "binary"); // バイナリーデータの場合はend()の第二引数に"binary"を指定
+            }
+        }
     }
 }
-new Main();
+const main = new Main();
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -1,27 +1,152 @@
 import * as http from "http";
+import * as fs from "fs";
+import * as path from "path";
+import { privateEncrypt } from "crypto";
 
 class Main {
-  constructor() {
-    // httpサーバーを設定する
-    const server: http.Server = http.createServer(
-      (request: http.IncomingMessage, response: http.ServerResponse) =>
-        this.requestHandler(request, response)
+    // rootFolder
+    private readonly _rootFolder: string;
+    constructor() {
+        // ルートフォルダーを指定
+        this._rootFolder = "/";
+        // httpサーバーを設定する
+        const server = http.createServer(
+            (request: http.IncomingMessage, response: http.ServerResponse) =>
+            this.requestListener(request, response)
     );
-    // サーバーを起動してリクエストを待ち受け状態にする
-    server.listen("5000");
-    // ログを出力する
-    console.log("http://localhost:5000 へアクセスください");
-  }
+        server.listen(process.env.PORT || 5000, () => this.listeningHandler());
+    }
+    /**
+     * httpサーバーが待ち受け状態になった時に実行される関数
+     */
+    private listeningHandler(): void{
+        console.log(
+            (process.env.PORT|| 5000) + "でhttpサーバーが待ち受け状態です"
+        );
+    }
 
-  /**
-   * サーバーにリクエストがあった時に実行される関数
-   */
-  private requestHandler(
+    private requestListener(
     request: http.IncomingMessage,
     response: http.ServerResponse
-  ): void {
-    response.end("Hello! Node.js with TypeScript");
-  }
-}
+    ): void {
+        // リクエストがあったファイル
+        let requestURL: string = request.url;
 
-new Main();
+        // リクエストのあったファイルの拡張子を取得
+        const extensionName: string = path.extname(requestURL);
+        let contentType: string;
+        let isBinary: boolean;
+
+        //ファイルの拡張子に応じてルーティング処理
+        if(extensionName != ""){
+            switch(extensionName){
+                case ".html":
+                    contentType = "text/html";
+                    isBinary = false;
+                    break;
+                case ".css":
+                    contentType = "text/css";
+                    isBinary = false;
+                    break;
+                case ".js":
+                case ".ts":
+                    contentType = "text/javascript";
+                    isBinary = false;
+                    break;
+                case ".png":
+                    contentType = "image/png";
+                    isBinary = true;
+                    break;
+                case ".jpg":
+                    contentType = "image/jpeg";
+                    isBinary = true;
+                    break;
+                case ".gif":
+                    contentType = "image/gif";
+                    isBinary = true;
+                    break;
+                case ".swf":
+                    contentType = "application/x-shockwave-flash";
+                    isBinary = true;
+                    break;
+                default:
+                    // どこにも該当しない場合は、index.htmlを読み込む
+                    requestURL = "index.html";
+                    contentType = "text/html";
+                    isBinary = false;
+                break;
+            }
+        }else{
+            //　拡張子が存在しない場合は、index.htmlを読み込む
+            requestURL = "index.html";
+            contentType = "text/html";
+            isBinary = false;
+        }
+        
+        this.readFileHandler(requestURL, contentType, isBinary, response);
+    }
+    private readFileHandler(
+        fileName: string,
+        contentType: string,
+        isBinary: boolean,
+        response: http.ServerResponse
+    ){
+        // ファイルの場所
+        const filePath: string = __dirname + this._rootFolder + fileName;
+        // filePathが存在するかどうかを調べる。存在している場合はexistにtrueが入る。
+        const exist = fs.existsSync(filePath);
+
+        this.responseHandler(exist, filePath, contentType, isBinary, response);
+    }
+
+    /**
+     * レスポンスを返す処理
+     */
+    private responseHandler(
+        exist: boolean,
+        filePath: string,
+        contentType: string,
+        isBinary: boolean,
+        response: http.ServerResponse
+    ){
+        if(exist){
+            // ファイルを読み込む際のエンコード指定
+            const encoding = !isBinary ? "utf8" : "binary";
+            // ファイルの読み込み
+            fs.readFile(
+                filePath,
+                { encoding: encoding },
+                (error: NodeJS.ErrnoException, data: string) =>
+                  this.fileReadHandler(error, data, contentType, isBinary, response)
+              );
+        } else {
+            //　ファイルが存在しない場合は400エラーを返す
+            response.statusCode = 400;
+            response.end("400 Error");
+        }
+    }
+    /**
+     * ファイルの読み込みが完了した時に実行される処理
+     */
+    private fileReadHandler(
+        error: NodeJS.ErrnoException,
+        data: string,
+        contentType: string,
+        isBinary: boolean,
+        response: http.ServerResponse
+      ): void {
+        if (error) {
+          response.statusCode = 500; // レスポンスデータにステータスコード500を設定
+          response.end("Internal Server Error");
+        } else {
+          response.statusCode = 200; // レスポンスデータにステータスコード200を設定
+          response.setHeader("Content-Type", contentType); // レスポンスデータのヘッダーにContent-Typeを設定
+          if (!isBinary) {
+            response.end(data);
+          } else {
+            response.end(data, "binary"); // バイナリーデータの場合はend()の第二引数に"binary"を指定
+          }
+        }
+      }
+    }
+const main = new Main();
